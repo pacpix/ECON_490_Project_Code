46b241a8a18c4df2aed2d2242b44deeed28d927a
Justin Stachofsky <stachofskyjustin@gmail.com>
2019-03-28 16:40:09 -0700

    Datapoint object creation working, still need to generate new CSV

---
+++ DataPoint.py
@@ -0,0 +1,17 @@
+"""
+DataPoint.py
+Class for DataPoints from stock CSV
+Last Edit: 3/28/2019 Justin Stachofsky
+
+"""
+
+class DataPoint:
+    
+    # Constructor
+    def __init__(self, date, efx_close, spy_close, tru_close):
+        self.date = date
+        self.efx_close = efx_close
+        self.spy_close = spy_close
+        self.tru_close = tru_close
+
+
--- Format_Dataset.py
+++ Format_Dataset.py
@@ -4,4 +4,33 @@ Purpose: Takes Yahoo finance closing prices csv and creates a new CSV to use for
 Last Edit: 3/27/2019 Justin Stachofsky
 """
 
+# Library imports
+import csv      # Python CSV functionalities
+
+# File Imports
+from DataPoint import DataPoint
+
+# Handle program flow
+def main():
+    
+     datapoints = create_datapoint_objects()
+
+     for datapoint in datapoints:
+        print(datapoint.efx_close + " " + datapoint.spy_close + " "+ datapoint.tru_close + " " + datapoint.date)
+
+def create_datapoint_objects():
+    
+    with open("Yahoo_Finance_Export.csv") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter =',')
+        datapoint_list = []
+
+        for row in csv_reader:
+            datapoint = DataPoint(row[0], row[1], row[2], row[3])
+            datapoint_list.append(datapoint)
+
+    return datapoint_list
+
+# Start program exection at main 
+if __name__== "__main__":
+  main()
 
