22325c7c5a7cdddf7f79aff21fb85bb71be0d2ab
Justin Stachofsky <stachofskyjustin@gmail.com>
2019-05-04 10:58:35 -0700

    Added simple regression model and the ability to use percent change instead of close price

--- Regression_Modeling/Equifax_DiD_Analysis.R
+++ Regression_Modeling/Equifax_DiD_Analysis.R
@@ -1,6 +1,6 @@
 # Equifax_DiD_Analysis.R
 # Purpose: Run DiD regressions for Equifax stock data
-# Last Edit: 3/30/2019 Justin Stachofsky
+# Last Edit: 5/4/2019 Justin Stachofsky
 
 
 # Package imported for setting working directory
@@ -9,8 +9,23 @@ install.packages("rstudioapi")
 # Set directory to location of script
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-# Function for configuring dataset dummy variables based on desired date ranges
-configureDataset <- function(csv_name, start_date, end_date)
+# Function for configuring dataset dummy variable for simple DiD
+configureDatasetOneDiD <- function(csv_name, start_date)
+{
+  # Import CSV
+  stock_data = read.csv(csv_name)
+  
+  # Create dummy variable for breach announcement
+  stock_data$Breach_Announced = ifelse(as.Date(stock_data$Date) >= start_date, 1, 0)
+  
+  # Interaction term for DiD
+  stock_data$BreachDiD = stock_data$Equifax * stock_data$Breach_Announced
+  
+  return(stock_data)
+}
+
+# Function for configuring dataset dummy variables based on desired date ranges for two DiD models
+configureDatasetTwoDiD <- function(csv_name, start_date, end_date)
 {
   # Import CSV
   stock_data = read.csv(csv_name)
@@ -27,28 +42,43 @@ configureDataset <- function(csv_name, start_date, end_date)
   
 }
 
-# Funcion builds difference in difference regression model for dataset
-buildDiDRegressionModel <- function(dataset)
+# Funcion builds one term difference in difference regression model for dataset
+buildRegressionModelOneDiD <- function(dataset)
 {
   # lm R function for fitting linear models
-  didreg = lm(Close.Price ~ Equifax + Initial_Period + Delayed_Period + InitialDiD + DelayedDiD, data = dataset )
-  
+  didreg = lm(Close_Price ~ Equifax + Breach_Announced + BreachDiD, data = dataset)
   return(didreg)
 }
 
-# Configure datasets for SPY and TRU regressions
-spy_wk_dataset <- configureDataset("Formatted_Export_spy.csv", as.Date("2017-09-07"), as.Date("2017-09-14"))
-spy_mo_dataset <- configureDataset("Formatted_Export_spy.csv", as.Date("2017-09-07"), as.Date("2017-10-07"))
-tru_wk_dataset <- configureDataset("Formatted_Export_tru.csv", as.Date("2017-09-07"), as.Date("2017-09-14"))
-tru_mo_dataset <- configureDataset("Formatted_Export_tru.csv", as.Date("2017-09-07"), as.Date("2017-10-07"))
+# Funcion builds two term difference in difference regression model for dataset
+buildRegressionModelTwoDiD <- function(dataset)
+{
+  # lm R function for fitting linear models
+  didreg = lm(Close_Price ~ Equifax + Initial_Period + Delayed_Period + InitialDiD + DelayedDiD, data = dataset )
+  return(didreg)
+}
 
-# Create regression models for SPY and TRU datasets
-spy_wk_did <- buildDiDRegressionModel(spy_wk_dataset)
-spy_mo_did <- buildDiDRegressionModel(spy_mo_dataset)
-tru_wk_did <- buildDiDRegressionModel(tru_wk_dataset)
-tru_mo_did <- buildDiDRegressionModel(tru_mo_dataset)
+# Configure datasets for regressions
+spy_simple_dataset <- configureDatasetOneDiD("Formatted_Export_spy.csv", as.Date("2017-09-08"))
+tru_simple_dataset <- configureDatasetOneDiD("Formatted_Export_tru.csv", as.Date("2017-09-08"))
+spy_wk_dataset <- configureDatasetTwoDiD("Formatted_Export_spy.csv", as.Date("2017-09-08"), as.Date("2017-09-15"))
+spy_mo_dataset <- configureDatasetTwoDiD("Formatted_Export_spy.csv", as.Date("2017-09-08"), as.Date("2017-10-08"))
+tru_wk_dataset <- configureDatasetTwoDiD("Formatted_Export_tru.csv", as.Date("2017-09-08"), as.Date("2017-09-15"))
+tru_mo_dataset <- configureDatasetTwoDiD("Formatted_Export_tru.csv", as.Date("2017-09-08"), as.Date("2017-10-08"))
+
+# Create DiD regression models
+spy_simple_did <- buildRegressionModelOneDiD(spy_simple_dataset)
+tru_simple_did <- buildRegressionModelOneDiD(tru_simple_dataset)
+spy_wk_did <- buildRegressionModelTwoDiD(spy_wk_dataset)
+spy_mo_did <- buildRegressionModelTwoDiD(spy_mo_dataset)
+tru_wk_did <- buildRegressionModelTwoDiD(tru_wk_dataset)
+tru_mo_did <- buildRegressionModelTwoDiD(tru_mo_dataset)
 
 # Print summary statistics for regression models
+print("SPY Simple")
+summary(spy_simple_did)
+print("TRU Simple")
+summary(tru_simple_did)
 print("SPY One Week")
 summary(spy_wk_did)
 print("SPY One Month")
