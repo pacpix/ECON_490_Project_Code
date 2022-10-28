b8c43ff664126b613ddaa968887a831e686c21e1
Julia Stachofsky <julia_stachofsky@protonmail.com>
2022-10-27 23:36:52 -0700

    Update Format_Dataset.py

--- CSV_Formatter/Format_Dataset.py
+++ CSV_Formatter/Format_Dataset.py
@@ -1,7 +1,7 @@
 """
 Format_Dataset.py
 Purpose: Takes Yahoo finance closing prices csv and creates a new CSV to use for R data analysis
-Last Edit: 5/2/2019 Justin Stachofsky
+Julia Stachofsky
 """
 
 # Library imports
